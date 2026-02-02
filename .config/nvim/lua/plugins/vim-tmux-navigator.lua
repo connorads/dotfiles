c7b17494c4cf66ba048476c1f5e77eed96388fe6
Connor Adams <connorads@users.noreply.github.com>
2026-02-02 21:17:09 +0000

    vim-tmux-navigator: seamless C-hjkl across nvim splits and tmux panes
    - Add vim-tmux-navigator nvim plugin with lazy-loading
    - Add import for local plugins/ dir in init.lua (was missing)
    - Replace M-hjkl with C-hjkl + is_vim passthrough in tmux
    - Pattern detects nvim, fzf, lazygit (incl nix -wrapped binaries)
    - Add copy-mode-vi bindings for pane nav in scroll mode

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+  },
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+  },
+}
