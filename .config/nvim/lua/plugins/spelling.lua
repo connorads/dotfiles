f80b418ed474b1fbe1f1ee1b2c55f50ed9080230
Connor Adams <connorads@users.noreply.github.com>
2026-02-15 14:33:35 +0000

    nvim: disable markdownlint for markdown.mdx filetype too
    LazyVim's markdown extra registers .mdx as "markdown.mdx" via on_very_lazy
    which overrides mdx.nvim's "mdx" filetype. Configure both filetypes in
    nvim-lint and conform to suppress markdownlint false positives (MD033,
    MD044) regardless of which filetype wins the loading race.

--- .config/nvim/lua/plugins/spelling.lua
+++ .config/nvim/lua/plugins/spelling.lua
@@ -1,6 +1,6 @@
 -- British English spell checking for prose filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "mdx", "astro", "text", "gitcommit" },
+  pattern = { "markdown", "mdx", "markdown.mdx", "astro", "text", "gitcommit" },
   callback = function()
     vim.opt_local.spell = true
     vim.opt_local.spelllang = "en_gb"
