8e0eb3c759b45982920129c2581c08b90a0836e7
Connor Adams <connorads@users.noreply.github.com>
2026-03-12 15:59:49 +0000

    add package manager aliases (bun, pnpm, npm)

---
+++ .config/zsh/aliases/pkg.zsh
@@ -0,0 +1,19 @@
+# Package managers
+alias b='bun'
+alias p='pnpm'
+alias n='npm'
+
+# exec variants
+alias bx='bunx'
+alias px='pnpx'
+alias nx='npx'
+
+# run variants
+alias br='bun run'
+alias pr='pnpm run'
+alias nr='npm run'
+
+# install variants
+alias bi='bun install'
+alias pi='pnpm install'
+alias ni='npm install'
